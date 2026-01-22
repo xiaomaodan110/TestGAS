fd937ca92c120613dc2aef03528b5e9ac19ed3ad
MARS-GP-刘义昭 <yizhao.liu@boomingtech.com>
2026-01-22 11:01:03 +0800

    some work time

--- Source/TestGAS/DamagedActor/TestGASHitBox.cpp
+++ Source/TestGAS/DamagedActor/TestGASHitBox.cpp
@@ -10,7 +10,15 @@ ATestGASHitBox::ATestGASHitBox(const FObjectInitializer& ObjectInitializer)
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	HitCollisionRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("CollisionRootComponent"));
+	RootComponent = HitCollisionRootComponent;
 
+	HitDamage = CreateDefaultSubobject<UBoxComponent>(TEXT("HitDamage"));
+	HitDamage->SetupAttachment(RootComponent);
+
+	InitialLifeSpan = 4.f;
+	bNetLoadOnClient = false;
+	bReplicates = false;
 }
 
 void ATestGASHitBox::HandleDamage(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
@@ -19,19 +27,32 @@ void ATestGASHitBox::HandleDamage(UPrimitiveComponent* OverlappedComp, AActor* O
 
 UPrimitiveComponent* ATestGASHitBox::GetHitDamage()
 {
-	return nullptr;
+	return HitDamage;
 }
 
 void ATestGASHitBox::SetHitDamageRelativePosition(const FVector& InNewPosition)
 {
+	if (UPrimitiveComponent* InHitComponent = GetHitDamage()) {
+		InHitComponent->SetRelativeLocation(InNewPosition);
+	}
 }
 
 void ATestGASHitBox::SetBoxExtent(const FVector& InNewBoxExtent)
 {
+	HitDamage->SetBoxExtent(InNewBoxExtent);
 }
 
 bool ATestGASHitBox::IsExist(ATestGASCharacterBase* InNewTarget) const
 {
+
+	for (auto& Tmp: AttackedTarget) {
+		if (Tmp.IsValid()){
+			if( Tmp == InNewTarget) {
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 
